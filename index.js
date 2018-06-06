f6265133a4e1198f0f54c969f95af0002c4e5a99
nadine ebrani <nadinedoshkeh3@gmail.com>
2018-06-06 03:42:38 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,24 @@
+function takeANumber(katzDeliLine, name){
+  var katzDeli = [];
+  for(i=0; i<10;i++){
+    console.log( "you are "+katzDeli[i]+"th in-line");
+  }
+}
+function nowServing(first){
+  for(i=0; i<line; i++){
+    
+  }else if(line=0){
+    console.log("There is nobody waiting to be served!")
+  }
+}
+function currentLine(katzLine){
+  katzLine=[]
+  for (i=1; i<katzLine.length; i++){
+    if(katzLine = i){
+      console.log("the line is currently:1."+katzLine[0]+", 2."+ katzLine[1])
+    }else if(katzLine = 0){
+      console.log("the line is empty");
+    }
+  }
+  return katzLine;
+}
